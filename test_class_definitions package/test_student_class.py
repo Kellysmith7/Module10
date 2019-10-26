84e613d3b2344f29045ac5b54fea21c7c4d8041a
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-26 16:59:08 -0500

    Fail major value error

--- test_class_definitions package/test_student_class.py
+++ test_class_definitions package/test_student_class.py
@@ -33,6 +33,10 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             student = sc.Student('Smith', '123456', 'CIS', 3.5)
 
+    def test_object_not_created_error_major(self):
+        with self.assertRaises(ValueError):
+            student = sc.Student('Smith', 'Joe', 22, 3.5)
+
 
 if __name__ == '__main__':
     unittest.main()
